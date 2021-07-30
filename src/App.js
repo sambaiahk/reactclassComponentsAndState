9b3cd273bf898b4e7732d682b19ac478a2f1be9d
sambaiahk <kattasambaiah3@gmail.com>
2021-07-30 06:22:44 +0000

    reactcl2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import Welcome from './components/Welcome'
+
+import Counter from './components/Counter'
+
+/*const App = () => {
+  return (
+    <div>
+      <Welcome name="Samba" />
+      <Welcome name="Santoshi" />
+    </div>
+  )
+}*/
+
+const App = () => <Counter />
+export default App
